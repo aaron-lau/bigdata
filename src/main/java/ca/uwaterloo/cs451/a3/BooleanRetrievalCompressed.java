b196103ef066a9ccb92c016b65793736e026c25e
Aaron Lau <aaronlau99@yahoo.ca>
2018-02-05 21:48:24 -0500

    a3: initial commit

---
+++ src/main/java/ca/uwaterloo/cs451/a3/BooleanRetrievalCompressed.java
@@ -0,0 +1,235 @@
+/**
+ * Bespin: reference implementations of "big data" algorithms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.uwaterloo.cs451.a3;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.MapFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
+import tl.lin.data.array.ArrayListWritable;
+import tl.lin.data.pair.PairOfInts;
+import tl.lin.data.pair.PairOfWritables;
+
+import java.util.Vector;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.fs.Path;
+import java.io.DataInputStream;
+import java.io.ByteArrayInputStream;
+import org.apache.hadoop.io.VIntWritable;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.WritableUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeSet;
+
+public class BooleanRetrievalCompressed extends Configured implements Tool {
+  private Vector <MapFile.Reader> index = new Vector <MapFile.Reader>();
+  private FSDataInputStream collection;
+  private Stack<Set<Integer>> stack;
+  private int numDirectory  = 0;
+
+  private BooleanRetrievalCompressed() {}
+
+  private void initialize(String indexPath, String collectionPath, FileSystem fs) throws IOException {
+
+    // Because of partitioning need to
+    Path path = new Path( indexPath );
+    ContentSummary cs = fs.getContentSummary(path);
+    numDirectory = (int)cs.getDirectoryCount() - 1;
+
+    for (int i =0; i < numDirectory; i++){
+      MapFile.Reader indexPart = new MapFile.Reader(new Path(indexPath + "/part-r-0000" + i), fs.getConf());
+      index.add(indexPart);
+    }
+    collection = fs.open(new Path(collectionPath));
+    stack = new Stack<>();
+  }
+
+  private void runQuery(String q) throws IOException {
+    String[] terms = q.split("\\s+");
+
+    for (String t : terms) {
+      if (t.equals("AND")) {
+        performAND();
+      } else if (t.equals("OR")) {
+        performOR();
+      } else {
+        pushTerm(t);
+      }
+    }
+
+    Set<Integer> set = stack.pop();
+
+    for (Integer i : set) {
+      String line = fetchLine(i);
+      System.out.println(i + "\t" + line);
+    }
+  }
+
+  private void pushTerm(String term) throws IOException {
+    stack.push(fetchDocumentSet(term));
+  }
+
+  private void performAND() {
+    Set<Integer> s1 = stack.pop();
+    Set<Integer> s2 = stack.pop();
+
+    Set<Integer> sn = new TreeSet<>();
+
+    for (int n : s1) {
+      if (s2.contains(n)) {
+        sn.add(n);
+      }
+    }
+
+    stack.push(sn);
+  }
+
+  private void performOR() {
+    Set<Integer> s1 = stack.pop();
+    Set<Integer> s2 = stack.pop();
+
+    Set<Integer> sn = new TreeSet<>();
+
+    for (int n : s1) {
+      sn.add(n);
+    }
+
+    for (int n : s2) {
+      sn.add(n);
+    }
+
+    stack.push(sn);
+  }
+
+  private Set<Integer> fetchDocumentSet(String term) throws IOException {
+    Set<Integer> set = new TreeSet<>();
+
+    for (PairOfInts pair : fetchPostings(term)) {
+      set.add(pair.getLeftElement());
+    }
+
+    return set;
+  }
+
+  private ArrayListWritable<PairOfInts> fetchPostings(String term) throws IOException {
+    Text key = new Text();
+    ArrayListWritable<PairOfInts> postingList = new ArrayListWritable<PairOfInts>();
+    int partition = (term.toString().hashCode() & Integer.MAX_VALUE) % numDirectory;
+
+    BytesWritable bytesData = new BytesWritable();
+    key.set(term);
+    index.elementAt(partition).get(key, bytesData);
+    byte[] bytes = bytesData.getBytes();
+    DataInputStream data = new DataInputStream(new ByteArrayInputStream(bytes));
+
+    int previousID = 0;
+    int docID = 0;
+    int termFrequency = 0;
+    boolean eof = true;
+    while (eof){
+      docID = WritableUtils.readVInt(data) ;
+      docID += previousID;
+      termFrequency = WritableUtils.readVInt(data);
+      if ((docID == previousID) || (termFrequency == 0)){
+        eof = false;
+      } else {
+        previousID = docID;
+        postingList.add(new PairOfInts(docID, termFrequency) );
+      }
+    }
+    return postingList;
+  }
+
+  public String fetchLine(long offset) throws IOException {
+    collection.seek(offset);
+    BufferedReader reader = new BufferedReader(new InputStreamReader(collection));
+
+    String d = reader.readLine();
+    return d.length() > 80 ? d.substring(0, 80) + "..." : d;
+  }
+
+  private static final class Args {
+    @Option(name = "-index", metaVar = "[path]", required = true, usage = "index path")
+    String index;
+
+    @Option(name = "-collection", metaVar = "[path]", required = true, usage = "collection path")
+    String collection;
+
+    @Option(name = "-query", metaVar = "[term]", required = true, usage = "query")
+    String query;
+  }
+
+  /**
+   * Runs this tool.
+   */
+  @Override
+  public int run(String[] argv) throws Exception {
+    final Args args = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
+
+    try {
+      parser.parseArgument(argv);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      return -1;
+    }
+
+    if (args.collection.endsWith(".gz")) {
+      System.out.println("gzipped collection is not seekable: use compressed version!");
+      return -1;
+    }
+
+    FileSystem fs = FileSystem.get(new Configuration());
+
+    initialize(args.index, args.collection, fs);
+
+    System.out.println("Query: " + args.query);
+    long startTime = System.currentTimeMillis();
+    runQuery(args.query);
+    System.out.println("\nquery completed in " + (System.currentTimeMillis() - startTime) + "ms");
+
+    return 1;
+  }
+
+  /**
+   * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
+   *
+   * @param args command-line arguments
+   * @throws Exception if tool encounters an exception
+   */
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new BooleanRetrievalCompressed(), args);
+  }
+}
---
+++ src/main/java/ca/uwaterloo/cs451/a3/BuildInvertedIndexCompressed.java
@@ -0,0 +1,257 @@
+/**
+ * Bespin: reference implementations of "big data" algorithms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.uwaterloo.cs451.a3;
+
+import io.bespin.java.util.Tokenizer;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MapFileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
+import tl.lin.data.array.ArrayListWritable;
+import tl.lin.data.fd.Object2IntFrequencyDistribution;
+import tl.lin.data.fd.Object2IntFrequencyDistributionEntry;
+import tl.lin.data.pair.PairOfInts;
+import tl.lin.data.pair.PairOfObjectInt;
+import tl.lin.data.pair.PairOfWritables;
+
+import org.apache.hadoop.io.WritableUtils;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.VIntWritable;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import tl.lin.data.pair.PairOfStringInt;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class BuildInvertedIndexCompressed extends Configured implements Tool {
+  private static final Logger LOG = Logger.getLogger(BuildInvertedIndexCompressed.class);
+
+  private static final class MyMapper extends Mapper<LongWritable, Text, PairOfStringInt, VIntWritable> {
+    private static final VIntWritable VINT = new VIntWritable();
+    private static final PairOfStringInt WORDandDOCNO = new PairOfStringInt();
+
+    private static final Object2IntFrequencyDistribution<String> COUNTS =
+        new Object2IntFrequencyDistributionEntry<>();
+
+    @Override
+    public void map(LongWritable docno, Text doc, Context context)
+        throws IOException, InterruptedException {
+      List<String> tokens = Tokenizer.tokenize(doc.toString());
+
+      // Build a histogram of the terms.
+      COUNTS.clear();
+      for (String token : tokens) {
+        COUNTS.increment(token);
+      }
+
+      // Emit postings.
+      for (PairOfObjectInt<String> e : COUNTS) {
+
+        // as discussed in clas place the doc number in the key
+        VINT.set(e.getRightElement() );
+        WORDandDOCNO.set(e.getLeftElement(), (int)docno.get());
+
+        context.write(WORDandDOCNO, VINT);
+      }
+    }
+  }
+
+  private static final class MyReducer extends
+      Reducer<PairOfStringInt, VIntWritable, Text, BytesWritable> {
+    private static final IntWritable DF = new IntWritable();
+    private static Text prevText ;
+    private static ArrayListWritable<PairOfInts> postingList ;
+    private static IntWritable previousDocnum;
+    private static BytesWritable bwritable ;
+    private static ByteArrayOutputStream bytearray ;
+    private static DataOutputStream data ;
+
+    @Override
+    public void setup(Context context)
+      throws IOException, InterruptedException {
+        postingList = new ArrayListWritable<PairOfInts>();
+        previousDocnum = new IntWritable();
+        previousDocnum.set(0);
+        prevText = new Text("");
+        bwritable = new BytesWritable();
+        bytearray = new ByteArrayOutputStream();
+        data = new DataOutputStream(bytearray);
+    }
+
+    @Override
+    public void reduce(PairOfStringInt key, Iterable<VIntWritable> values, Context context)
+        throws IOException, InterruptedException {
+      ArrayListWritable<PairOfInts> postings = new ArrayListWritable<>();
+
+      String term = key.getLeftElement();
+
+      // if key.term != prev and prev != null
+      if ((!term.equals(prevText.toString())) && (prevText.getLength() != 0 )){
+
+        for (PairOfInts pair : postingList){
+          WritableUtils.writeVInt(data, pair.getLeftElement() ); //key.doc id
+          WritableUtils.writeVInt(data, pair.getRightElement()); //term frequency
+        }
+
+        bwritable.set(bytearray.toByteArray(), 0, bytearray.size());
+        // emit(key.term, postings)
+        context.write(prevText, bwritable);
+        // postings.reset()
+        postingList.clear();
+        bwritable.setSize(0);
+        bytearray.reset();
+        data.flush();
+        previousDocnum.set(0);
+      }
+
+      Iterator<VIntWritable> iter = values.iterator();
+      //reduce   append
+      int df = 0;
+      while (iter.hasNext()) {
+        df = df + iter.next().get();
+      }
+      postingList.add(new PairOfInts(key.getRightElement() - (int)previousDocnum.get() , df));
+
+      prevText.set(key.getLeftElement());
+      previousDocnum.set(key.getRightElement());
+    }
+
+    // emit (prev, postings)
+    @Override
+    public void cleanup(Context context) throws IOException, InterruptedException{
+        for (PairOfInts pair : postingList){
+          WritableUtils.writeVInt(data, pair.getLeftElement()  ); //left doc num
+          WritableUtils.writeVInt(data, pair.getRightElement()); //right term frequency
+        }
+
+        bwritable.set(bytearray.toByteArray(), 0, bytearray.size());
+
+        int currDocnum = (int)previousDocnum.get();
+        if (currDocnum != 0 ){
+          context.write(prevText, bwritable);
+        }
+
+        postingList.clear();
+        bytearray.reset();
+        data.flush();
+        bwritable.setSize(0);
+        prevText.set("");
+
+      }
+    }
+
+
+  protected static class MyPartitioner extends Partitioner<PairOfStringInt, VIntWritable> {
+    @Override
+    public int getPartition(PairOfStringInt key, VIntWritable value, int numReduceTasks) {
+      return (key.getLeftElement().toString().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+    }
+  }
+
+  private BuildInvertedIndexCompressed() {}
+
+  private static final class Args {
+    @Option(name = "-input", metaVar = "[path]", required = true, usage = "input path")
+    String input;
+
+    @Option(name = "-output", metaVar = "[path]", required = true, usage = "output path")
+    String output;
+
+    @Option(name = "-reducers", metaVar = "[num]", required = false, usage = "number of reducers")
+    public int numReducers = 1;
+  }
+
+  /**
+   * Runs this tool.
+   */
+  @Override
+  public int run(String[] argv) throws Exception {
+    final Args args = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
+
+    try {
+      parser.parseArgument(argv);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      return -1;
+    }
+
+    LOG.info("Tool: " + BuildInvertedIndexCompressed.class.getSimpleName());
+    LOG.info(" - input path: " + args.input);
+    LOG.info(" - output path: " + args.output);
+    LOG.info(" - num reducers: " + args.numReducers);
+
+    Job job = Job.getInstance(getConf());
+    job.setJobName(BuildInvertedIndexCompressed.class.getSimpleName());
+    job.setJarByClass(BuildInvertedIndexCompressed.class);
+
+    job.setNumReduceTasks(args.numReducers);
+
+    FileInputFormat.setInputPaths(job, new Path(args.input));
+    FileOutputFormat.setOutputPath(job, new Path(args.output));
+
+    job.setMapOutputKeyClass(PairOfStringInt.class);
+    job.setMapOutputValueClass(VIntWritable.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(BytesWritable.class);
+    job.setOutputFormatClass(MapFileOutputFormat.class);
+
+    job.setMapperClass(MyMapper.class);
+    job.setReducerClass(MyReducer.class);
+    job.setPartitionerClass(MyPartitioner.class);
+
+    // Delete the output directory if it exists already.
+    Path outputDir = new Path(args.output);
+    FileSystem.get(getConf()).delete(outputDir, true);
+
+    long startTime = System.currentTimeMillis();
+    job.waitForCompletion(true);
+    System.out.println("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
+
+    return 0;
+  }
+
+  /**
+   * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
+   *
+   * @param args command-line arguments
+   * @throws Exception if tool encounters an exception
+   */
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new BuildInvertedIndexCompressed(), args);
+  }
+}
