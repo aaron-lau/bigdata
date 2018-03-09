20bdb93d7d68d4507a71d264476f49014eccaa71
Aaron Lau <aaronlau99@yahoo.ca>
2018-03-09 14:26:56 -0500

    a5: adding Q3

---
+++ src/main/scala/ca/uwaterloo/cs451/a5/Q3.scala
@@ -0,0 +1,114 @@
+package ca.uwaterloo.cs451.a5
+import io.bespin.scala.util.Tokenizer
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+import org.apache.spark.Partitioner
+import java.util.StringTokenizer
+import scala.collection.JavaConverters._
+import org.apache.spark.sql.SparkSession
+
+// class Conf(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
+//   // mainOptions = Seq(input, date, )
+//   val input = opt[String](descr = "input path", required = true)
+//   val date = opt[String](descr = "l_shipdate", required = true)
+//   val text = opt[Boolean](descr = "parquet", required = false)
+//   val parquet = opt[Boolean](descr = "parquet", required = false)
+//   verify()
+// }
+//
+// class MyPartitioner(numOfPar: Int) extends Partitioner {
+//   def numPartitions: Int = numOfPar
+//   def getPartition(key: Any): Int = {
+//     val k = key.asInstanceOf[(String, String)]
+//     ((k._1.hashCode() & Integer.MAX_VALUE) % numPartitions)
+//   }
+// }
+
+object Q3 extends Tokenizer {
+  val log = Logger.getLogger(getClass().getName())
+
+  def main(argv: Array[String]) {
+    val args = new Conf(argv)
+
+    log.info("Input: " + args.input())
+    log.info("Date: " + args.date())
+
+    val conf = new SparkConf().setAppName("Q1")
+    val sc = new SparkContext(conf)
+    val shipdate = args.date()
+    // val lineitemRDD =
+
+    if (args.text()){
+      val part = sc.textFile(args.input() + "/part.tbl")
+        .map(line => (line.split("""\|""")(0), line.split("""\|""")(1)))
+
+      val partBroadcast = sc.broadcast(part.collectAsMap())
+
+      val supplier = sc.textFile(args.input() + "/supplier.tbl")
+        .map(line => (line.split("""\|""")(0), line.split("""\|""")(1)))
+
+      val supplierBroadcast = sc.broadcast(supplier.collectAsMap())
+
+      val results = sc.textFile(args.input() + "/lineitem.tbl")
+        .map(line => (line, line.split("""\|""")(10)))
+        .filter(_._2.substring(0, shipdate.length()) == shipdate)
+        .map(pair => {
+          val orderkey = pair._1.split("""\|""")(0)
+          val partkey = pair._1.split("""\|""")(1)
+          val suppkey = pair._1.split("""\|""")(2)
+          val partTable = partBroadcast.value.get(partkey)
+          val supplierTable = supplierBroadcast.value.get(suppkey)
+          (orderkey.toLong, (partTable,supplierTable))
+        })
+        .sortByKey(true)
+        .take(20)
+
+      results.foreach{ pair=>
+        pair match{
+          case (orderkey,(Some(partkey),Some(suppkey))) => println("("+orderkey+","+partkey+","+suppkey+")")
+          case _ =>  println()
+        }
+      }
+    }
+    else if (args.parquet()){
+      val sparkSession = SparkSession.builder.getOrCreate
+      val partDF = sparkSession.read.parquet(args.input()+"/part")
+      val partRDD = partDF.rdd
+      val partRDDFilterd = partRDD
+        .map(part => (part.getInt(0).toString, part.getString(1)))
+      val partBroadcast = sc.broadcast(partRDDFilterd.collectAsMap())
+
+      val supplierDF = sparkSession.read.parquet(args.input()+"/supplier")
+      val supplierRDD = supplierDF.rdd
+      val supplierRDDFilterd = supplierRDD
+        .map(supplier => (supplier.getInt(0).toString, supplier.getString(1)))
+      val supplierBroadcast = sc.broadcast(supplierRDDFilterd.collectAsMap())
+
+      val lineitemDF = sparkSession.read.parquet(args.input()+"/lineitem")
+      val results = lineitemDF.rdd
+        .map(line => (line, line.getString(10)))
+        .filter(_._2.substring(0, shipdate.length()) == shipdate)
+        .map(pair => {
+          val orderkey = pair._1.getInt(0).toString
+          val partkey = pair._1.getInt(1).toString
+          val suppkey = pair._1.getInt(2).toString
+          val partTable = partBroadcast.value.get(partkey)
+          val supplierTable = supplierBroadcast.value.get(suppkey)
+          (orderkey.toLong, (partTable,supplierTable))
+        })
+        .sortByKey(true)
+        .take(20)
+
+      results.foreach{ pair=>
+        pair match{
+          case (orderkey,(Some(partkey),Some(suppkey))) => println("("+orderkey+","+partkey+","+suppkey+")")
+          case _ =>  println()
+        }
+      }
+
+    }
+  }
+}
