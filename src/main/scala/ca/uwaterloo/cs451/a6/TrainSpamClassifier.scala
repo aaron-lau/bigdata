091540b69cca387590d2cc91f62633c9df8ec636
Aaron Lau <aaronlau99@yahoo.ca>
2018-03-26 12:22:09 -0400

    a6: fix shuffling

--- src/main/scala/ca/uwaterloo/cs451/a6/TrainSpamClassifier.scala
+++ src/main/scala/ca/uwaterloo/cs451/a6/TrainSpamClassifier.scala
@@ -15,7 +15,7 @@ class Conf(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
   // mainOptions = Seq(input, date, )
   val input = opt[String](descr = "input path", required = true)
   val model = opt[String](descr = "model path", required = true)
-  val shuffle = opt[Boolean](descr = "to shuffle?", required = false)
+  val shuffle = opt[Boolean](descr = "to shuffle?", required = false, default = Option(false))
   verify()
 }
 
@@ -27,6 +27,7 @@ object TrainSpamClassifier extends Tokenizer {
 
     log.info("Input: " + args.input())
     log.info("Model: " + args.model())
+    log.info("Shuffle: " + args.shuffle())
 
     val conf = new SparkConf().setAppName("TrainSpamClassifier")
     val sc = new SparkContext(conf)
@@ -34,9 +35,8 @@ object TrainSpamClassifier extends Tokenizer {
     FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
 
     val shuffle = args.shuffle()
-    //TO DO
 
-    val trained = sc.textFile(args.input()).map(line => {
+    val trained = if (!shuffle) {sc.textFile(args.input()).map(line => {
       // Parse input
       val instanceArray = line.split(" ")
       val docid = instanceArray(0)
@@ -46,19 +46,25 @@ object TrainSpamClassifier extends Tokenizer {
       }
       val features = instanceArray.slice(2, instanceArray.length).map { featureIndex => featureIndex.toInt }
       (0, (docid, isSpam, features))
-    })
-
-    if (!shuffle) {
-      trained.coalesce(1)
-    } else if (shuffle) {
-      trained.map(pair => {
+    }).coalesce(1).groupByKey(1).persist()} else {
+      sc.textFile(args.input()).map(line => {
+        // Parse input
+        val instanceArray = line.split(" ")
+        val docid = instanceArray(0)
+        var isSpam = 0
+        if (instanceArray(1).equals("spam")) {
+          isSpam = 1
+        }
+        val features = instanceArray.slice(2, instanceArray.length).map { featureIndex => featureIndex.toInt }
+        (0, (docid, isSpam, features))
+      }).map(pair => {
           val r = scala.util.Random
           (r.nextInt(), pair._2)
         })
         .sortByKey()
         .map(pair => (0, pair._2))
+        .groupByKey(1).persist()
     }
-    trained.groupByKey(1).persist()
 
     // w is the weight vector (make sure the variable is within scope)
     var w = Map[Int, Double]()
@@ -74,8 +80,10 @@ object TrainSpamClassifier extends Tokenizer {
     val delta = 0.002
 
     val new_w = trained.mapPartitions(indexIterator => {
-          indexIterator.foreach(instanceIterable => {
-          val tuple = instanceIterable._2
+          // indexIterator.foreach(instanceIterable => {
+          // val tuple = instanceIterable._2
+          val instanceIterable = indexIterator.next._2
+          instanceIterable.foreach(tuple => {
           // For each instance...
           // label
           val isSpam = tuple._2
